fd7b5463d874959b9b00399330b3a49172b37b8d
Chance Guyton <chanceguyton@gmail.com>
2025-06-11 21:26:58 -0400

    Update main.py
    Update backend to respond to fetch requests by frontend url

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # or ["*"] for dev
+    allow_origins=["https://stock-pulse-brown.vercel.app"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
